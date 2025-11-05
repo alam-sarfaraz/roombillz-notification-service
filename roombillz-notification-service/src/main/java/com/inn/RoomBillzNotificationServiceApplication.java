1551a528050caa7c139a1111ff20289a4e916921
sarfaraz alam <sarfarazalam2702@gmail.com>
2025-11-05 23:24:21 +0530

    NotificationService - Day-4

--- roombillz-notification-service/src/main/java/com/inn/RoomBillzNotificationServiceApplication.java
+++ roombillz-notification-service/src/main/java/com/inn/RoomBillzNotificationServiceApplication.java
@@ -2,6 +2,7 @@ package com.inn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -15,6 +16,7 @@ import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 @EnableAspectJAutoProxy
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @OpenAPIDefinition(
--- roombillz-notification-service/src/main/java/com/inn/client/RoomBillzFeignClient.java
+++ roombillz-notification-service/src/main/java/com/inn/client/RoomBillzFeignClient.java
@@ -2,8 +2,7 @@ package com.inn.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "ROOMBILLZ-SERVICE", url = "${roombillz.service.url}",
-             configuration = {FeignConfig.class,FeignRetryConfig.class})
+@FeignClient(name = "ROOMBILLZ-SERVICE",configuration = {FeignConfig.class,FeignRetryConfig.class})
 public interface RoomBillzFeignClient {
 
 }

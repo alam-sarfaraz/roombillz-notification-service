7b51a4a706a242bbe1cc1259f451ca1ff0bed0ff
sarfaraz alam <sarfarazalam2702@gmail.com>
2025-11-08 22:56:06 +0530

    NotificationService - Day-6.2

--- roombillz-notification-service/src/main/java/com/inn/service/impl/EventMessageServiceImpl.java
+++ roombillz-notification-service/src/main/java/com/inn/service/impl/EventMessageServiceImpl.java
@@ -9,12 +9,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.inn.converter.JsonUtil;
 import com.inn.dto.EventMessageCreateRequestDto;
+import com.inn.dto.EventMessageDTO;
+import com.inn.dto.PurchaseOrderDetailNotificationEvent;
 import com.inn.dto.ResponseDto;
 import com.inn.entity.EventMessage;
 import com.inn.notificationConstants.NotificationServiceConstant;
 import com.inn.repository.IEventMessageRepository;
 import com.inn.service.IEventMessageService;
+import com.inn.service.IPurchaseOrderDetailService;
 
 @Service
 public class EventMessageServiceImpl implements IEventMessageService {
@@ -23,6 +27,9 @@ public class EventMessageServiceImpl implements IEventMessageService {
 	
 	@Autowired
 	IEventMessageRepository eventMessageRepository;
+	
+	@Autowired
+	IPurchaseOrderDetailService purchaseOrderDetailService;
 	
 	@Override
 	public ResponseEntity<ResponseDto> createEvent(EventMessageCreateRequestDto eventMessageCreateRequestDto) {
@@ -47,7 +54,7 @@ public class EventMessageServiceImpl implements IEventMessageService {
 	public EventMessage findEarliestFailedEvent() {
 		logger.info(NotificationServiceConstant.INSIDE_THE_METHOD +"findEarliestFailedEvent");
 		try {
-			return eventMessageRepository.findFirstByStatusOrderByCreatedAtAsc("Failed").orElse(null);
+			return eventMessageRepository.findFirstByStatusOrderByCreatedAtAsc(NotificationServiceConstant.FAILED).orElse(null);
 		} catch (Exception e) {
 			logger.error(NotificationServiceConstant.ERROR_OCCURRED_DUE_TO, kv(NotificationServiceConstant.ERROR_MESSAGE, e.getMessage()));
 			throw e;
@@ -84,11 +91,38 @@ public class EventMessageServiceImpl implements IEventMessageService {
 	        }
 
 	        Integer id = earliestFailedEvent.getId();
-	        this.updateEventMessageByStatus(id, "Success");
+	        parsedDataForEntity(earliestFailedEvent);
+	        this.updateEventMessageByStatus(id, NotificationServiceConstant.SUCCESS);
 	        return ResponseEntity.ok(new ResponseDto("200", "EventMessage sent to Notification Service successfully."));
 	    } catch (Exception e) {
 	        logger.error(NotificationServiceConstant.ERROR_OCCURRED_DUE_TO, kv(NotificationServiceConstant.ERROR_MESSAGE, e.getMessage()), e);
 	        throw e;
 	    }
+	}
+	
+	public void parsedDataForEntity(EventMessage eventMessage) {
+		logger.info(NotificationServiceConstant.INSIDE_THE_METHOD +"parsedDataForEntity");
+		String eventType = eventMessage.getEventType();
+		logger.info("EventType received from RoomBillz Service: {}", kv("EventType",eventType));
+		switch (eventType) {
+		case NotificationServiceConstant.PURCHASE_ORDER_CREATED:
+			createPurchaseOrderDetail(eventMessage.getMessage());
+			break;
+		case "Approved":
+			System.out.println("Order is approved");
+			break;
+		case "Rejected":
+			System.out.println("Order is rejected");
+			break;
+		default:
+			System.out.println("Unknown status");
+		}
+	}
+	
+	public void createPurchaseOrderDetail(String message) {
+		logger.info(NotificationServiceConstant.INSIDE_THE_METHOD + "createPurchaseOrderDetail");
+		PurchaseOrderDetailNotificationEvent purchaseOrderDetailNotifiEvent = JsonUtil.fromJson(message,PurchaseOrderDetailNotificationEvent.class);
+		logger.info("EventType received from RoomBillz Service: {}", kv("PurchaseId",purchaseOrderDetailNotifiEvent.getPurchaseId()));
+		purchaseOrderDetailService.createPurchaseOrder(purchaseOrderDetailNotifiEvent);
 	}
 }
